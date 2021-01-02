651d08b8c186e4f26bd09b22c631918686f17536
MajorForce <74123905+MajorForce@users.noreply.github.com>
2021-01-02 03:22:16 +0200

    Add files via upload

---
+++ Lesson01.01.py
@@ -0,0 +1,15 @@
+a = 123             # int
+q = 456.78          # float
+w = False           # bool
+t = 'hundred'       # str
+
+print(w, a)
+print(t, q)
+
+name = input("Введите имя: ")
+height = float(input("Введите рост в м: "))
+weight = int(input("Введите вес в кг: "))
+
+print(name, type(name))
+print(height, type(height))
+print(weight, type(weight))
